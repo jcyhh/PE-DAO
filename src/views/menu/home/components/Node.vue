4d9196514f432aca80e6e8cfca1a33be1c77b791
jcyhh <2824193088@qq.com>
2025-08-20 15:40:47 +0800

    8.20

--- src/views/menu/home/components/Node.vue
+++ src/views/menu/home/components/Node.vue
@@ -8,7 +8,7 @@
             <div class="flex jb ac">
                 <div class="flex ac">
                     <img :src="nodeIcon1" class="img44 mr20" :class="'icon anibc'">
-                    <div class="size24 bold">{{ '身份-宝剑' }}</div>
+                    <div class="size24 bold">身份-{{ '宝剑' }}</div>
                 </div>
                 <div>
                     <div class="size24 opc6 mb20">进度 50%</div>
@@ -44,9 +44,9 @@
             <div class="flex jb ac">
                 <div class="flex ac">
                     <img :src="item.icon" class="img44 mr20" :class="item.className">
-                    <div class="size24 bold">{{ item.name }}</div>
+                    <div class="size24 bold">身份-{{ item.name }}</div>
                 </div>
-                <div class="btn" @click="submit">立即购买</div>
+                <div class="btn" @click="show=true">立即购买</div>
             </div>
             <div class="size40 bold mt40">500.00</div>
             <div class="size20 blue mt4">售卖价格(USDT)</div>
@@ -72,6 +72,30 @@
         </div>
     </div>
 
+    <van-popup v-model:show="show" style="background-color: transparent !important;" overlay-class="cusMask">
+        <div class="pop">
+            <div class="flex jb ac">
+                <div class="linearTxt size32 bold">成为宝剑</div>
+                <van-icon name="cross" color="#999999" :size="25" @click="show=false" />
+            </div>
+            <div class="size40 bold mt60">500.00</div>
+            <div class="size20 blue mt4">售卖价格(USDT)</div>
+            <div class="flex jb ac mt30">
+                <div class="size24 opc6">空投代币比例</div>
+                <div class="size26">40%</div>
+            </div>
+            <div class="flex jb ac mt30">
+                <div class="size24 opc6">释放周期</div>
+                <div class="size26">8个月</div>
+            </div>
+            <div class="flex jb ac mt30">
+                <div class="size24 opc6">释放方式</div>
+                <div class="size26">10%/10%/5%/5%...</div>
+            </div>
+            <div class="tips size20 mt60 lh48">提示:PE是由区块链爱好者为了维护所有数字资产持有者的链上财产主权而发起的DAO项目，与任何组织或个人无关。购买NFT将被视为认同PE理念而主动赞助项目发展的行为，不涉及任何投资;</div>
+            <div class="mainBtn mt40">确认</div>
+        </div>
+    </van-popup>
 </template>
 
 <script setup lang="ts">
@@ -79,11 +103,14 @@ import nodeIcon1 from '@/assets/imgs/8.png'
 import nodeIcon2 from '@/assets/imgs/10.png'
 import nodeIcon3 from '@/assets/imgs/11.png'
 import CusProgress from '@/components/CusProgress/index.vue'
+import { ref } from 'vue'
+
+const show = ref(false)
 
 const list = [
-    { name: '身份-宝剑', icon: nodeIcon1 , className:'icon anibc' },
-    { name: '身份-权杖', icon: nodeIcon2, className:'icon anibr' },
-    { name: '身份-皇冠', icon: nodeIcon3, className:'crow' }
+    { name: '宝剑', icon: nodeIcon1 , className:'icon anibc' },
+    { name: '权杖', icon: nodeIcon2, className:'icon anibr' },
+    { name: '皇冠', icon: nodeIcon3, className:'crow' }
 ]
 
 const submit = () => {
@@ -98,5 +125,15 @@ const submit = () => {
     left: -30px;
     top: 343px;
     z-index: 1;
+}
+.pop{
+    width: 590px;
+    padding: 30px 30px 40px 30px;
+    border: 1px solid #FFFFFF80;
+    border-radius: 20px;
+    background: linear-gradient(52deg, #1C1F1D, #080908);
+    .tips{
+        color: #EED05F;
+    }
 }
 </style>
