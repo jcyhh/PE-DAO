66fc5713a4d0c0b91fcb6b9b1be0defc98cdc44c
2824193088@qq.com <2824193088@qq.com>
2025-10-29 22:21:17 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -6,60 +6,60 @@
         <component :is="Component" v-if="!$route.meta.keepAlive" />
     </RouterView>
 
-    <CusLoading v-model="loading"></CusLoading>
+    <!-- <CusLoading v-model="loading"></CusLoading> -->
 </template>
 
 <script setup lang="ts">
-import { startPath } from '@/config/dapp';
-import { useEthers } from '@/dapp';
-import { routerReplace } from './router';
-import { useDappStore } from './store';
-import { storeToRefs } from 'pinia';
-import CusLoading from '@/components/CusLoading/index.vue'
-import { delToken } from './config/storage';
+// import { startPath } from '@/config/dapp';
+// import { useEthers } from '@/dapp';
+// import { routerReplace } from './router';
+// import { useDappStore } from './store';
+// import { storeToRefs } from 'pinia';
+// import CusLoading from '@/components/CusLoading/index.vue'
+// import { delToken } from './config/storage';
 
-const dappStore = useDappStore()
-const { hasMetaMask, address, loading } = storeToRefs(dappStore)
+// const dappStore = useDappStore()
+// const { hasMetaMask, address, loading } = storeToRefs(dappStore)
 
-const { ethereum } = window as any;
+// const { ethereum } = window as any;
 
-/**
- * 钱包处理
- */
-const { checkMetaMask, connectWallet, checkChain } = useEthers()
+// /**
+//  * 钱包处理
+//  */
+// const { checkMetaMask, connectWallet, checkChain } = useEthers()
 
-const init = async () => {
-    hasMetaMask.value = await checkMetaMask()
-    if(hasMetaMask.value==1){
-        const { address:walletAddress } = await connectWallet() // 连接钱包
-        await checkChain() // 检查网络
-        createListener() // 创建监听
-        address.value = walletAddress
-    }else{
-        address.value = ''
-    }
-}
+// const init = async () => {
+//     hasMetaMask.value = await checkMetaMask()
+//     if(hasMetaMask.value==1){
+//         const { address:walletAddress } = await connectWallet() // 连接钱包
+//         await checkChain() // 检查网络
+//         createListener() // 创建监听
+//         address.value = walletAddress
+//     }else{
+//         address.value = ''
+//     }
+// }
 
-// 创建监听
-const createListener = () => {
-    ethereum.on('accountsChanged', handlerChanged); // 账户切换或断开钱包链接
-    ethereum.on('chainChanged',  handlerChanged); // 网络切换
-}
-// 移除监听
-const removeListener = () => {
-    ethereum.off('accountsChanged', handlerChanged);
-    ethereum.off('chainChanged',  handlerChanged);
-}
-// 回调：账户切换、断开钱包链接、网络切换
-const handlerChanged = async () => {    
-    address.value = ''
-    delToken()
-    removeListener();
-    routerReplace(startPath)
-    await init();
-}
+// // 创建监听
+// const createListener = () => {
+//     ethereum.on('accountsChanged', handlerChanged); // 账户切换或断开钱包链接
+//     ethereum.on('chainChanged',  handlerChanged); // 网络切换
+// }
+// // 移除监听
+// const removeListener = () => {
+//     ethereum.off('accountsChanged', handlerChanged);
+//     ethereum.off('chainChanged',  handlerChanged);
+// }
+// // 回调：账户切换、断开钱包链接、网络切换
+// const handlerChanged = async () => {    
+//     address.value = ''
+//     delToken()
+//     removeListener();
+//     routerReplace(startPath)
+//     await init();
+// }
 
-init()
+// init()
 </script>
 
 <style scoped></style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -5,69 +5,12 @@
     <div class="start flex jc ac">
         <img src="@/assets/common/start.png" class="startImg animate__animated animate__bounceInRight" />
     </div>
-    <div class="size28 mt30 grey tips animate__animated animate__fadeInUp" v-if="hasMetaMask==2">{{ $t('请安装') }} MateMask !</div>
-
-    <van-popup v-model:show="show" style="background-color: transparent !important;" :close-on-click-overlay="false" overlay-class="cusMask">
-        <div class="bind">
-            <div class="tc size32 bold">{{ $t('激活地址') }}</div>
-            <div class="mt60 size24">{{ $t('当前地址') }}</div>
-            <div class="box mt20 size24 gray br lh30">{{ address }}</div>
-            <div class="mt30 size24">{{ $t('推荐码') }}</div>
-            <div class="box mt20 flex">
-                <input type="text" v-model="inviteCode" :placeholder="$t('请输入推荐码')" class="size24  flex1">
-            </div>
-            <div class="mainBtn mt40" v-scale v-delay="{fun:loginIn}">{{ $t('确认') }}</div>
-        </div>
-    </van-popup>
+    <div class="size28 mt30 grey tips animate__animated animate__fadeInUp">{{ $t('系统更新中...') }}</div>
 </template>
 
 <script setup lang="ts">
-import { homePath } from '@/config/dapp';
-import { getToken, setToken } from '@/config/storage';
-import { useEthers } from '@/dapp';
-import { routerReplace } from '@/router';
-import { useDappStore } from '@/store';
-import { apiGet, apiPost } from '@/utils/request';
-import { storeToRefs } from 'pinia';
-import { ref, watch } from 'vue';
 import CusLang from '@/components/CusLang/index.vue';
 
-const { getSign, connectWallet } = useEthers()
-
-const dappStore = useDappStore()
-const { address, hasMetaMask } = storeToRefs(dappStore)
-
-const show = ref(false)
-const inviteCode = ref()
-
-// 当钱包对象异步注入到浏览器后，钱包登录 或者 去首页
-watch(address, async (val)=>{
-    if(!val)return
-    setTimeout(() => getToken() ? routerReplace(homePath) : checkIsRegister(), 1500);
-},{immediate:true})
-
-const checkIsRegister = () => {
-    apiGet('/api/auth/is_reg',{
-        address: address.value
-    }).then((res:any)=>{
-        if(res.is_reg==1)loginIn()
-        else show.value = true
-    })
-}
-
-// 钱包登录
-const loginIn = async () => {
-    await connectWallet()
-    const signInfo = await getSign('Login')
-    apiPost('/api/auth/login',{
-        ref: inviteCode.value,
-        address: address.value,
-        ...signInfo
-    }).then((res:any)=>{
-        setToken(res.token)
-        routerReplace(homePath)
-    })
-}
 </script>
 
 <style lang="scss" scoped>
